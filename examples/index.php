354d66f1753cdce7c4c506a30f168ede73c0433a
Daniel Bulant <admin@danbulant.eu>
2020-03-10 15:01:15 +0100

    Example containgin title and subtitle as well

--- examples/index.php
+++ examples/index.php
@@ -8,19 +8,27 @@ error_reporting(E_ALL);
 require "../autoInclude.php";
 
 use widgets\document;
-use widgets\head\title;
+use widgets\head\title as HeadTitle;
 use widgets\head\responsiveness;
 use widgets\head\styles;
 use widgets\widgetList;
 use widgets\clickables\button;
+use widgets\dataholders\title;
+use widgets\dataholders\subtitle;
 
 $document = new Document([
     "head" => new WidgetList([
-        new Title("PHP Flutter"),
+        new HeadTitle("PHP Flutter"),
         new Responsiveness(),
         new Styles()
     ]),
     "body" => new WidgetList([
+        new Title([
+            "text" => "Hey there!"
+        ]),
+        new Subtitle([
+            "text"=> "PHP Flutter is great!"
+        ]),
         new Button([
             "text" => "Test"
         ])
