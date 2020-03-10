ff0a96e953423a54a9e6defa995ca1f127cb2976
Daniel Bulant <admin@danbulant.eu>
2020-03-10 12:16:56 +0100

    Fix path

--- autoInclude.php
+++ autoInclude.php
@@ -1,7 +1,7 @@
 <?
 spl_autoload_register(function ($class) {
     $file = str_replace('\\', DIRECTORY_SEPARATOR, $class) . '.php';
-    if (file_exists($file)) {
+    if (file_exists(__DIR__ . DIRECTORY_SEPARATOR . $file)) {
         require __DIR__ . DIRECTORY_SEPARATOR . $file;
         return true;
     }
