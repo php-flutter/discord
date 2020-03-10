ba13ea80eeb17cd8a84038a1e6bff51b69c12603
Daniel Bulant <admin@danbulant.eu>
2020-03-10 17:51:21 +0100

    Add support for custom attributes

--- widgets/clickables/button.php
+++ widgets/clickables/button.php
@@ -15,13 +15,16 @@ class button extends widget
         $this->elmn = @$this->obj->element ?: "button";
         $this->variant = @$this->obj->variant ?: "blue";
         $this->text = @$this->obj->text ?: "";
+        $this->action = @$this->obj->action ?: "";
+
+        $this->attributes = $this->generateAttributes($opt, ["element", "variant", "text", "action"]);
     }
     /**
      * Build the document
      */
     function build(){
         ?>
-            <<?=$this->elmn; ?> class="button button-variant-<?=$this->variant; ?>"><?=$this->textOrBuild($this->text); ?></<?=$this->elmn; ?>>
+            <<?=$this->elmn; ?> onclick="<?=$this->action; ?>" class="button button-variant-<?=$this->variant; ?>" <?=$this->attributes; ?>><?=$this->textOrBuild($this->text); ?></<?=$this->elmn; ?>>
         <?
     }
 
